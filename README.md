ead14b4b3dbfb4e7556eda646ec945eced4d55fe
Zener_085 <57841939+Zener085@users.noreply.github.com>
2021-12-17 11:31:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LearningC
+ It's repository, where I'll save my codes from c++ learning. I'm learning it by "Bro Code" and CodeWars' tasks
---
+++ cmake-build-debug/CMakeFiles/LearningC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearningC.dir/main.cpp.obj"
+  "LearningC.exe"
+  "LearningC.exe.manifest"
+  "LearningC.lib"
+  "LearningC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearningC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
