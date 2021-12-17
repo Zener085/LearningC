bf297739bf6b422c90a2dc6bcb73b72aa1e47c88
Zener_085 <57841939+Zener085@users.noreply.github.com>
2021-12-17 18:19:05 +0300

    Create 1 task from CodeWars
    Idk how it works, I had to solve it using `count_if`, but my compiler didn't let use it

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string>
+
+int getCount(const std::string& inputStr){
+    int num_vowels = std::count(inputStr.begin(), inputStr.end(), 'a');
+    num_vowels += std::count(inputStr.begin(), inputStr.end(), 'e');
+    num_vowels += std::count(inputStr.begin(), inputStr.end(), 'u');
+    num_vowels += std::count(inputStr.begin(), inputStr.end(), 'i');
+    num_vowels += std::count(inputStr.begin(), inputStr.end(), 'o');
+    return num_vowels;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string f;
+    getline(std::cin, f);
+    std::cout << getCount(f) << std::endl;
     return 0;
 }
