43df92429577e6ab2cf5aab54080945b4bac7268
Zener_085 <57841939+Zener085@users.noreply.github.com>
2021-12-19 15:43:33 +0300

    Task "Who likes it?"

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-/// My algorithm
-int centuryFromYear(int year) {
-    if (year % 100 != 0)
-        return year / 100 + 1;
-
-    return year / 100;
-}
-
-/// The best algorithm from answers
-int centuryFromYearEdited(int year) {
-    return (year + 99) / 100;
+/// Best solution from CodeWars
+std::string likes(const std::vector<std::string> &names){
+    switch(names.size()){
+        case 0: return "no one likes this";
+        case 1: return names[0] + " likes this";
+        case 2: return names[0] + " and " + names[1] + " like this";
+        case 3: return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        default: return names[0] + ", " + names[1] + " and " + std::to_string(names.size() - 2) + " others like this";
+    }
 }
 
 int main() {
-    int a;
-    std::cin >> a;
-    std::cout << centuryFromYear(a) << centuryFromYearEdited(a) << std::endl;
+    int n;
+    std::cin >> n;
+    std::vector<std::string> names;
+    for (int i = 0; i < n; i++){
+        std::string a;
+        std::cin >> a;
+        names.push_back(a);
+    }
+    std::cout << likes(names) << std::endl;
     return 0;
 }
