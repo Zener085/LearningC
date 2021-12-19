fde030132e7f271a5b56da56e56f229cba9a7cdd
Zener_085 <57841939+Zener085@users.noreply.github.com>
2021-12-19 16:38:14 +0300

    Task "Convert a String to a Number!"

--- main.cpp
+++ main.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
-#include <vector>
 #include <string>
 
-/// Best solution from CodeWars
-std::string likes(const std::vector<std::string> &names){
-    switch(names.size()){
-        case 0: return "no one likes this";
-        case 1: return names[0] + " likes this";
-        case 2: return names[0] + " and " + names[1] + " like this";
-        case 3: return names[0] + ", " + names[1] + " and " + names[2] + " like this";
-        default: return names[0] + ", " + names[1] + " and " + std::to_string(names.size() - 2) + " others like this";
-    }
+/// The best solution from CodeWars
+int string_to_number(const std::string& s) {
+    return std::stoi(s);
+}
+
+/// My solution
+int own_string_to_number(const std::string& s) {
+    return std::atoi(s.c_str());
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    std::vector<std::string> names;
-    for (int i = 0; i < n; i++){
-        std::string a;
-        std::cin >> a;
-        names.push_back(a);
-    }
-    std::cout << likes(names) << std::endl;
+    std::string a;
+    std::cin >> a;
+    std::cout << string_to_number(a) << std::endl;
     return 0;
 }
