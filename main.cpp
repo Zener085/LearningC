1b747a7caee43c9b1d6be254c4620d2ea4e8e85f
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-01-31 21:56:19 +0300

    Create frequency check file
    Create header file for frequency analysis, for shunting yard algorithm and combine all in one directory

--- main.cpp
+++ main.cpp
@@ -1,25 +1,14 @@
 #include <iostream>
+#include "shuntingYardAlgorithm.h"
+#include "FrequencyAnalysis.h"
 
-void insertion_sort(int* a, int n){
-    int key, j, i;
-    for (i = 1; i < n; i++){
-        key = a[i];
-        j = i - 1;
-        while (j >= 0 and a[j] > key){
-            a[j + 1] = a[j];
-            j--;
-        }
-        a[j + 1] = key;
-    }
-}
+using namespace std;
+
 
 int main() {
-    int n;
-    std::cin >> n;
-    int* arr = new int[n];
-    for (int i = 0; i < n; i++)
-        std::cin >> arr[i];
-    insertion_sort(arr, n);
-    for (int i = 0; i < n; i++)
-        std::cout << arr[i] << " ";
+    frequency_analysis();
+
+    shunting_yard();
+
+    return 0;
 }
