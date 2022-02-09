4fa22c8bd6419262fca33a085362c0249f2c0d9f
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-02-09 17:20:15 +0300

    hard fibonacci problem
    find all numbers till 100000th element with O(logn) sort

--- main.cpp
+++ main.cpp
@@ -3,32 +3,35 @@
 #define ll long long
 
 namespace fibonacci {
-    ll easy_find(const ll& number) {
-        if (number <= 1) return number;
-        return easy_find(number - 1) + easy_find(number - 2);
-    }
+//    ll easy_find(const ll& number) {
+//        if (number <= 1) return number;
+//        return easy_find(number - 1) + easy_find(number - 2);
+//    }
 
-//    ll* numbers;
-//
-//    ll find(const ll& number) {
-//        if (numbers[number] > 0) return numbers[number];
-//        ll answer = find(number - 1) + find(number - 2);
-//        numbers[number] = answer;
-//        return answer;
-//    }
-//
-//    ll find_number(const ll& number) {
-//        numbers = new ll[number];
-//        numbers[0] = numbers[1] = 1;
-//        for (int i = 2; i < number; i++)
-//            numbers[i] = 0;
-//        return find(number);
-//    }
+    ll numbers[100000] = {0};
+
+    ll find(const ll& number) {
+        if (number == 0)
+            return 0;
+        if (number == 1 || number == 2)
+            return (numbers[number] = 1);
+
+        if (numbers[number])
+            return numbers[number];
+
+        ll k = (number & 1)? (number + 1) / 2 : number / 2;
+
+        numbers[number] = (number & 1)
+                        ? (find(k)*find(k) + find(k-1)*find(k-1))
+                        : (2*find(k-1) + find(k))*find(k);
+
+        return numbers[number];
+    }
 }
 
 int main() {
     ll a;
     std::cin >> a;
-    std::cout << fibonacci::easy_find(a);
+    std::cout << fibonacci::find(a);
     return 0;
 }
