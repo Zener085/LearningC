3466d9bcc9ad30b33edce649cfcb12c8eb18e828
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-02-09 16:53:06 +0300

    Pyramid using namespace
    Add namespace for draw() function

--- main.cpp
+++ main.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 
-void pyramid(const int& height){
-    for (int i = 2; i <= height * 2; i+=2) {
-        for (int j = 0; j < (height * 2 - i) / 2; j++)
-            cout << " ";
-        for (int j = 0; j < i; j++){
-            cout << "*";
+namespace pyramid {
+    void draw(const int &height) {
+        for (int i = 2; i <= height * 2; i += 2) {
+            for (int j = 0; j < (height * 2 - i) / 2; j++)
+                cout << " ";
+            for (int j = 0; j < i; j++) {
+                cout << "*";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
 }
 
 int main() {
     int a;
     cin >> a;
-    pyramid(a);
+    pyramid::draw(a);
 
     return 0;
 }
