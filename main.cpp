0da30eb38486700932e485f343cfb2699d3fef4c
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-02-09 17:09:50 +0300

    fibonacci problem
    easy solution for fibonacci problem

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 
-namespace pyramid {
-    void draw(const int &height) {
-        for (int i = 2; i <= height * 2; i += 2) {
-            for (int j = 0; j < (height * 2 - i) / 2; j++)
-                cout << " ";
-            for (int j = 0; j < i; j++) {
-                cout << "*";
-            }
-            cout << endl;
-        }
+#define ll long long
+
+namespace fibonacci {
+    ll easy_find(const ll& number) {
+        if (number <= 1) return number;
+        return easy_find(number - 1) + easy_find(number - 2);
     }
+
+//    ll* numbers;
+//
+//    ll find(const ll& number) {
+//        if (numbers[number] > 0) return numbers[number];
+//        ll answer = find(number - 1) + find(number - 2);
+//        numbers[number] = answer;
+//        return answer;
+//    }
+//
+//    ll find_number(const ll& number) {
+//        numbers = new ll[number];
+//        numbers[0] = numbers[1] = 1;
+//        for (int i = 2; i < number; i++)
+//            numbers[i] = 0;
+//        return find(number);
+//    }
 }
 
 int main() {
-    int a;
+    ll a;
     std::cin >> a;
-    pyramid::draw(a);
-
+    std::cout << fibonacci::easy_find(a);
     return 0;
 }
