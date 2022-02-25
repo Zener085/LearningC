10e4bb0c13061e5e38340261fa65a21494b23298
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-02-25 11:08:13 +0300

    Implement new task
    Program reads 2 strings from input file - 1 main string and string of words, that main string contains.  Then it returns indices where substring starts, substring contains all words from the second string

---
+++ SSAD/FindSumArray.h
@@ -0,0 +1,86 @@
+#ifndef SSAD_FINDSUMARRAY_H
+#define SSAD_FINDSUMARRAY_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+/// calculate actual size of array
+///@param arr array of numbers, bad array
+///@param n size of bad array
+///@return size of good array
+int actual_size(const int* (arr), const int n) {
+    int size = 0; // actual size
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) size++; // increase size till finding first 0 in array
+    }
+
+    return size;
+}
+
+/// parsing string to create array of numbers
+///@param str string with numbers
+///@param size (reference) size of string
+///@return array of numbers
+int* parse_string(const std::string& str, int& size) {
+    int* long_arr = new int[size]; // temporal array, save only numbers, but with large size, call it bad array
+    int sign = 1; // sign of number which is needed to store
+    int k = 0; // index in temporal array
+
+    for (int i = 0; i < str.length(); i++) long_arr[i] = 0; // make 0s to all array
+
+    // take symbols one by one and store numbers from the string
+    for (char c : str) {
+        if (c == '-') sign = -1;
+        if ('0' <= c && c <= '9') {
+            long_arr[k++] = (c - '0') * sign;
+            sign = 1;
+        }
+    }
+
+    size = actual_size(long_arr, (int) str.length()); // calculate normal size of array
+    int* right_arr = new int[size]; // create array with good size, call it good array
+
+    for (int i = 0; i < size; i++) right_arr[i] = long_arr[i]; // copy elements from the bad array to the good array
+
+    return right_arr;
+}
+
+
+/// find maximum sum from numbers array
+///@param arr string from input.txt file
+///@return maximum sum from array
+int max_sum_subarray(const std::string& arr) {
+    int sum, current_sum; // maximum sum and current sum
+
+    int size = (int) arr.length(); // size of array
+    int* nums = parse_string(arr, size); // parsed array of numbers
+
+    sum = current_sum = nums[0]; // take first element from array
+
+    for (int i = 1; i < size; i++) { // find maximum sum
+        current_sum = std::max(nums[i], current_sum + nums[i]);
+        sum = std::max(sum, current_sum);
+    }
+
+    return sum;
+}
+
+/// read string from input.txt and write maximum sum to the output.txt
+[[maybe_unused]] void in_out_file() {
+    std::ifstream input; // input file
+    std::ofstream output; // output file
+    std::string numbers; // string of numbers
+
+    input.open("input.txt");
+    std::getline(input, numbers); // read string of numbers
+    input.close();
+
+    output.open("output.txt");
+    output << max_sum_subarray(numbers); // write maximum sum of numbers array
+    output.close();
+}
+
+
+#endif //SSAD_FINDSUMARRAY_H
--- SSAD/main.cpp
+++ SSAD/main.cpp
@@ -1,81 +1,85 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <unordered_map>
 
-/// calculate actual size of array
-///@param arr array of numbers, bad array
-///@param n size of bad array
-///@return size of good array
-int actual_size(const int* (arr), const int n) {
-    int size = 0; // actual size
+///Creator vector of words from one string, taken from input file
+///@param str string with words
+///@return vector of words
+std::vector<std::string> parse_string_to_vector(const std::string& str){
+    std::vector<std::string> words; // vector of words
+    std::string word; // one word
 
-    for (int i = 0; i < n; i++) {
-        if (arr[i] != 0) size++; // increase size till finding first 0 in array
-    }
-
-    return size;
-}
-
-/// parsing string to create array of numbers
-///@param str string with numbers
-///@param size (reference) size of string
-///@return array of numbers
-int* parse_string(const std::string& str, int& size) {
-    int* long_arr = new int[size]; // temporal array, save only numbers, but with large size, call it bad array
-    int sign = 1; // sign of number which is needed to store
-    int k = 0; // index in temporal array
-
-    for (int i = 0; i < str.length(); i++) long_arr[i] = 0; // make 0s to all array
-
-    // take symbols one by one and store numbers from the string
-    for (char c : str) {
-        if (c == '-') sign = -1;
-        if ('0' <= c && c <= '9') {
-            long_arr[k++] = (c - '0') * sign;
-            sign = 1;
+    for (auto c : str) {
+        if (c == ' ') { // if word is ended
+            words.push_back(word);
+            word = "";
+            continue;
         }
+        word += c; // else increase word
     }
+    words.push_back(word); // add the last word
 
-    size = actual_size(long_arr, (int) str.length()); // calculate normal size of array
-    int* right_arr = new int[size]; // create array with good size, call it good array
-
-    for (int i = 0; i < size; i++) right_arr[i] = long_arr[i]; // copy elements from the bad array to the good array
-
-    return right_arr;
+    return words;
 }
 
+/// Create and return vector of indices, where there is substring starts
+///@param str main string
+///@return vector of indices, where each substring starts
+std::vector<int> substring(const std::string& str, const std::string& words_) {
+    std::vector<std::string> words = parse_string_to_vector(words_); // vector of all words the program has to find
+    std::vector<int> result; // vector of indices of starting substring
 
-/// find maximum sum from numbers array
-///@param arr string from input.txt file
-///@return maximum sum from array
-int max_sum_subarray(const std::string& arr) {
-    int sum, current_sum; // maximum sum and current sum
+    // size of one word, number of words in vector words and size of all substring
+    int size = (int) words[0].size(), count = (int) words.size(), all_size = size * count;
 
-    int size = (int) arr.length(); // size of array
-    int* nums = parse_string(arr, size); // parsed array of numbers
+    if (all_size > str.size()) // if the main string is smaller than size of all words in vector
+        return result;
 
-    sum = current_sum = nums[0]; // take first element from array
+    std::unordered_map<std::string, int> hash_words; // stores words and indices of them
 
-    for (int i = 1; i < size; i++) { // find maximum sum
-        current_sum = std::max(nums[i], current_sum + nums[i]);
-        sum = std::max(sum, current_sum);
+    for (int i = 0; i < count; i++) // initialize all words indices in hash map
+        hash_words[words[i]]++;
+
+    for (int i = 0; i <= str.size() - all_size; i++) {
+        // create temp variables
+        std::unordered_map<std::string, int> temp_hash(hash_words); // hash map
+        int j = i, temp_count = count; // index of loop and number of words
+
+        while (j < i + all_size) {
+            std::string word = str.substr(j, size); // decrease size of string to 1 word
+
+            if (hash_words.find(word) == hash_words.end() || temp_hash[word] == 0) // if we do not find word
+                break;
+            else {
+                --temp_hash[word]; // else decrease for this word calculations
+                --temp_count;
+            }
+            j += size;
+        }
+        if (temp_count == 0) // if the program found all words in substring
+            result.push_back(i); // store index of start the iteration calculation
     }
-
-    return sum;
+    return result;
 }
 
 /// read string from input.txt and write maximum sum to the output.txt
 void in_out_file() {
     std::ifstream input; // input file
     std::ofstream output; // output file
-    std::string numbers; // string of numbers
+    std::string str, null_str, words; // main string, for skipping string and string with words
 
     input.open("input.txt");
-    std::getline(input, numbers); // read string of numbers
+    std::getline(input, str); // read main string
+    std::getline(input, null_str); // just skip one string
+    std::getline(input, words); // read words that it has to find
     input.close();
 
     output.open("output.txt");
-    output << max_sum_subarray(numbers); // write maximum sum of numbers array
+    std::vector<int> indices = substring(str, words);
+    for (int index : indices) // write all indices
+        output << index << " ";
     output.close();
 }
 
